eb1240b61aefda7fa80eaa73e0618e245bd6fa39
shashank <shashankp846@gmail.com>
2026-04-19 16:47:57 +0530

    join the frontend and backend

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -18,7 +18,7 @@ const connectDB = async ()=>{
     }
     catch(error){
         console.error(`MongoDB connection error: ${error.message}`);
-        process.exit(1);
+        throw error;
     }
 }
 
